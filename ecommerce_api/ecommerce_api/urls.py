bbc2e13541ad667e9a4bc5d665bbfa666c737bb7
rdeekshitha184-ship-it <rdeekshitha184@gmail.com>
2026-01-30 20:27:37 +0530

    Added JWT token authentication endpoints

--- ecommerce_api/ecommerce_api/urls.py
+++ ecommerce_api/ecommerce_api/urls.py
@@ -17,6 +17,11 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -24,5 +29,12 @@ urlpatterns = [
     path('api/', include('cart.urls')),
     path('api/', include('orders.urls')),
     path('api/', include('accounts.urls')),
+     path('admin/', admin.site.urls),
+
+    # JWT Auth
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
+    path('api/', include('cart.urls')),
 
 ]
